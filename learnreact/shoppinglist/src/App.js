e997d02986959ae01c93480a32ea77bfdf3469cc
Mohd Abdul Mannan <abdulmannan@Mohds-MacBook-Air.local>
2023-09-08 07:07:54 +0530

    8 sep

---
+++ learnreact/shoppinglist/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Product from './Components/Product/Product'
+const App = () => {
+  return (
+    <div className="main">
+     <Product/>
+    </div>
+  )
+
+} 
+
+export default App;  
---
+++ learnreact/shoppinglist/src/Components/Product/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Product = () => {
+
+  return (
+    <div>
+        <p>
+            This is React project
+        </p>
+    </div>
+  )
+}
+
+export default Product;
